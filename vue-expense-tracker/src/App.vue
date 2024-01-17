abfe024e13cb9e71b3c337dd8c9fe3f59501acd3
Nenad Shkurtevski <nenad.shkurtevski1@gmail.com>
2024-01-17 12:03:24 +0100

    update document

--- vue-expense-tracker/src/App.vue
+++ vue-expense-tracker/src/App.vue
@@ -1,1 +1,20 @@
-<template>My App</template>
+<template>
+  <AppHeader />
+  <div class="container">
+    <AppBalance />
+    <IncomeExpenses />
+  </div>
+</template>
+
+<script lang="ts">
+import AppHeader from './components/AppHeader.vue'
+import AppBalance from './components/AppBalance.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+export default {
+  components: {
+    AppHeader,
+    AppBalance,
+    IncomeExpenses
+  }
+}
+</script>
