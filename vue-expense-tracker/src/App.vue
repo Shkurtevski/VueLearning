ab937003de6fb552cd76186c557d7e95bdffce3a
Nenad Shkurtevski <nenad.shkurtevski1@gmail.com>
2024-01-17 12:31:49 +0100

    Options API added for example

--- vue-expense-tracker/src/App.vue
+++ vue-expense-tracker/src/App.vue
@@ -3,6 +3,8 @@
   <div class="container">
     <AppBalance />
     <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
   </div>
 </template>
 
@@ -10,11 +12,16 @@
 import AppHeader from './components/AppHeader.vue'
 import AppBalance from './components/AppBalance.vue'
 import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+
 export default {
   components: {
     AppHeader,
     AppBalance,
-    IncomeExpenses
+    IncomeExpenses,
+    TransactionList,
+    AddTransaction
   }
 }
 </script>
---
+++ vue-expense-tracker/src/components/TransactionList.vue
@@ -0,0 +1,30 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <!----- OPTIONS-API-RENDERING-METHOD ----->
+    <!-- <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
+    </li> -->
+  </ul>
+</template>
+
+<!----- OPTIONS-API ----->
+<script lang="ts">
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: 'Flower', amount: -19.99 },
+        { id: 2, text: 'Salary', amount: 299.97 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 }
+      ]
+    }
+  }
+}
+</script>
